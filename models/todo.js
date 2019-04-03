4487ece299b75b2f6fb009759704972e3da70cf7
peterjast <peterjast@yahoo.com>
2019-04-03 12:01:07 -0700

    yee

--- models/todo.js
+++ models/todo.js
@@ -1,24 +1,22 @@
-module.exports = function(sequelize, DataTypes){
-  var ToDo = sequelize.define("ToDo",{
-      task: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate:{
-              len:[1,255]
-          }
-      },
-      completed: {
-          type: DataTypes.BOOLEAN,
-          defaultValue: false
+module.exports = function(sequelize, DataTypes) {
+  var ToDo = sequelize.define("ToDo", {
+    task: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 255]
       }
-
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
   });
 
-  ToDo.associate = function(models){
-      ToDo.belongsTo(models.Accounts, {
-          foreignKey: "owner_id" 
-      });
+  ToDo.associate = function(models) {
+    ToDo.belongsTo(models.Accounts, {
+      foreignKey: "owner_id"
     });
-      
+  };
   return ToDo;
 };
